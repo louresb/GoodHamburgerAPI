ba6291f9184fb74fead8ea16e61e275c15f291f7
Bruno Loures <bruno.loures@hotmail.com>
2025-04-30 20:21:57 -0300

    feat: configure in-memory database and define core entities

---
+++ src/GoodHamburger.Domain/DTOs/OrderRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace GoodHamburger.Domain.DTOs;
+
+public class OrderRequest
+{
+    public int? SandwichId { get; set; }
+    public int? FriesId { get; set; }
+    public int? SoftDrinkId { get; set; }
+}
